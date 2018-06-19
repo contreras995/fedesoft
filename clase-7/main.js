6abb0035213403a398a84e9d53554682d85b8890
Ramiro Contreras <contreras995@yahoo.es>
2018-06-19 18:54:33 -0500

    ejercicio 2 y 3

--- clase-7/main.js
+++ clase-7/main.js
@@ -1,3 +1,13 @@
 function cambiarStyle() {
-    document.getElementById('pMensaje').style.fontSize = "45px";        
+    document.getElementById('pMensaje').style.fontSize = "45px"; 
+    
 }
+
+function sumarRestar (params) {
+    
+    var arreglo = [5,2];
+    resultado = arreglo[0] +  arreglo[1];
+
+    alert("Suma = "+ params +": Resultado" + resultado);
+}
+
