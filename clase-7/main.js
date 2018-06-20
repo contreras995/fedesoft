4185f1946b0ccbb12139c08a38342947c737e2f2
Ramiro Contreras <contreras995@yahoo.es>
2018-06-19 19:21:45 -0500

    ejercicio 6

--- clase-7/main.js
+++ clase-7/main.js
@@ -30,3 +30,46 @@ function ejercicio4(params) {
 
 }
 
+function ejercicio5() {
+
+    var visible = true;
+    alert(!visible);  // Muestra "false" y no "true"
+    var cantidad = 0;
+    var resultado = !cantidad;  // resultado = true
+    alert(resultado);
+    cantidad = 2;
+    resultado = !cantidad;  // resultado = false
+    alert(resultado);
+    var mensaje = "";
+    resultado = !mensaje;  // resultado = true
+    alert(resultado);
+    var valor1 = true;
+    var valor2 = false;
+    resultado = valor1 && valor2; // resultado = false
+    alert(resultado);
+    valor1 = true;
+    valor2 = true;
+    resultado = valor1 && valor2; // resultado = true
+    alert(resultado);
+    var valor1 = true;
+    var valor2 = false;
+    resultado = valor1 || valor2; // resultado = true
+    alert(resultado);
+    valor1 = false;
+    valor2 = false;
+    resultado = valor1 || valor2; // resultado = false
+    alert(resultado);
+
+    
+}
+
+function ejercicio6(suma1, suma2, resta, multiplicacion,division) {
+    
+    var sumar = suma1 + suma2;
+    var restar =sumar- resta;
+    var multiplicar = sumar * multiplicacion;
+    var dividir = sumar/division;
+    var operacionModulo = sumar %division;
+    alert("SUMA " + sumar + " Resta " + restar + " multiplicacion " + multiplicar + " dividir " + dividir + " muodulo " + operacionModulo);
+
+}
