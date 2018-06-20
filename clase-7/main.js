f086d051c65da7718be403f1a0b68296092854a2
Ramiro Contreras <contreras995@yahoo.es>
2018-06-19 19:00:10 -0500

    ejercicio 4

--- clase-7/main.js
+++ clase-7/main.js
@@ -11,3 +11,22 @@ function sumarRestar (params) {
     alert("Suma = "+ params +": Resultado" + resultado);
 }
 
+function ejercicio4(params) {
+    var numero1 = 3;
+    var numero2 = 4;
+    
+    // Ahora, la variable numero1 vale 5
+    numero1 = 5;
+    
+    // Ahora, la variable numero1 vale 4
+    numero1 = numero2;
+    
+    var resultado = 5;
+
+    resultado++;
+    alert(resultado);
+    resultado--; 
+    alert(resultado);
+
+}
+
